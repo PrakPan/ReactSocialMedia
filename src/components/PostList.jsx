1b63e2872e33b262b2a71e17b9adc3e48b29a14a
Prakhar <prakharsomu014@gmail.com>
2024-01-07 23:46:49 +0530

    useEffect & Loader added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,19 +1,32 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListContext } from "../store/post-store";
 import WelcomeMessage from "./WelcomeMessage";
+import Loader from "./Loader";
 
 const PostList =()=>{
   const {postList,addInitialPosts}= useContext(PostListContext);
-  const handlePosts=()=>{
-    fetch('https://dummyjson.com/posts')
-    .then(res=>res.json())
-    .then(data=>addInitialPosts(data.posts));
-  }
+  const [fetched,setFetched] = useState(false);
+
+  useEffect(()=>{
+    const controller = new AbortController();
+    const signal = controller.signal;
+   fetch('https://dummyjson.com/posts',{ signal })
+  .then(res=>res.json())
+  .then(data=>{
+    addInitialPosts(data.posts)
+    setFetched(true)})
+ 
+    return ()=>{
+      controller.abort();
+    };
+  },[]);
+
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage onGetPosts={handlePosts}/>}
-      {postList.map((post)=> <Post key={post.id} post={post}></Post>)}
+      {!fetched && <Loader/>}
+      {fetched && postList.length === 0 && <WelcomeMessage />}
+      {fetched && postList.map((post)=> <Post key={post.id} post={post}></Post>)}
     </>
   )
 }
